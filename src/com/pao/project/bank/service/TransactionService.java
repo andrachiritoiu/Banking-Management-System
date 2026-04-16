e91df7370a15a2c61a1f6776e1d6010d62e29c83
andrachiritoiu <chiritoiu.andra@yahoo.com>
2026-04-16 21:40:47 +0300

    Add AccountService

--- src/com/pao/project/bank/service/TransactionService.java
+++ src/com/pao/project/bank/service/TransactionService.java
@@ -1,4 +1,27 @@
 package com.pao.project.bank.service;
 
+import com.pao.project.bank.model.transaction.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TransactionService {
+    private static final TransactionService INSTANCE = new TransactionService();
+
+    private final List<Transaction> transactions = new ArrayList<>();
+
+    private TransactionService() {}
+
+    public static TransactionService getInstance() {
+        return INSTANCE;
+    }
+
+
+    public void recordTransaction(Transaction transaction) {
+        if (transaction == null) {
+            throw new IllegalArgumentException("Transaction cannot be null.");
+        }
+
+        transactions.add(transaction);
+    }
 }
